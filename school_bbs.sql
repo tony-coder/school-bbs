755a7b342b46710b2d67c3ed870223fa93311b30
tony-coder <1606771552@qq.com>
2020-01-09 00:28:38 +0800

    更新 最终版数据库

--- school_bbs.sql
+++ school_bbs.sql
@@ -1,161 +1,223 @@
-/*
- Navicat Premium Data Transfer
+CREATE DATABASE  IF NOT EXISTS `school_bbs` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `school_bbs`;
+-- MySQL dump 10.13  Distrib 8.0.16, for Win64 (x86_64)
+--
+-- Host: localhost    Database: school_bbs
+-- ------------------------------------------------------
+-- Server version	8.0.16
 
- Source Server         : Mysql
- Source Server Type    : MySQL
- Source Server Version : 80016
- Source Host           : localhost:3306
- Source Schema         : school_bbs
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
- Target Server Type    : MySQL
- Target Server Version : 80016
- File Encoding         : 65001
+--
+-- Table structure for table `best_topic`
+--
 
- Date: 10/12/2019 22:40:14
-*/
-
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
-
--- ----------------------------
--- Table structure for best_topic
--- ----------------------------
 DROP TABLE IF EXISTS `best_topic`;
-CREATE TABLE `best_topic`  (
-  `id` int(11) NOT NULL COMMENT '精华帖id',
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `best_topic` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '精华帖id',
   `topic_id` int(11) NOT NULL COMMENT '对应帖子id',
   `user_id` int(11) NOT NULL COMMENT '对应用户id',
-  `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建日期',
-  `state` int(4) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE,
-  INDEX `fk_bestTopic_topic`(`topic_id`) USING BTREE,
-  INDEX `fk_bestTopic_user`(`user_id`) USING BTREE,
+  `create_time` datetime DEFAULT NULL COMMENT '创建日期',
+  `state` int(4) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_bestTopic_topic` (`topic_id`),
+  KEY `fk_bestTopic_user` (`user_id`),
   CONSTRAINT `fk_bestTopic_topic` FOREIGN KEY (`topic_id`) REFERENCES `topic` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `fk_bestTopic_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Table structure for black_list
--- ----------------------------
+--
+-- Table structure for table `black_list`
+--
+
 DROP TABLE IF EXISTS `black_list`;
-CREATE TABLE `black_list`  (
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `black_list` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '黑名单id',
   `user_id` int(11) NOT NULL COMMENT '黑名单中的用户id',
   `level` int(4) NOT NULL COMMENT '屏蔽等级',
   PRIMARY KEY (`id`) USING BTREE,
-  INDEX `fk_blacklist_user`(`user_id`) USING BTREE,
+  KEY `fk_blacklist_user` (`user_id`) USING BTREE,
+  CONSTRAINT `FK5bflr4wamndlkhhcrp07mopyx` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
   CONSTRAINT `fk_blacklist_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Table structure for login_log
--- ----------------------------
+--
+-- Table structure for table `login_log`
+--
+
 DROP TABLE IF EXISTS `login_log`;
-CREATE TABLE `login_log`  (
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `login_log` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '日志id',
   `user_id` int(11) NOT NULL COMMENT '用户id',
-  `login_time` datetime(0) NOT NULL COMMENT '登陆时间',
-  `ip` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '登陆IP',
-  `device` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '设备',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+  `login_time` datetime NOT NULL COMMENT '登陆时间',
+  `ip` varchar(30) NOT NULL COMMENT '登陆IP',
+  `device` varchar(255) DEFAULT NULL COMMENT '设备',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Table structure for main_section
--- ----------------------------
+--
+-- Table structure for table `main_section`
+--
+
 DROP TABLE IF EXISTS `main_section`;
-CREATE TABLE `main_section`  (
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `main_section` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主板块id',
   `title` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '主板块标题',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+  `info` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Table structure for notice
--- ----------------------------
+--
+-- Table structure for table `notice`
+--
+
 DROP TABLE IF EXISTS `notice`;
-CREATE TABLE `notice`  (
-  `id` int(11) NOT NULL COMMENT '公告id',
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `notice` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '公告id',
   `user_id` int(11) NOT NULL COMMENT '发布公告的管理员id',
-  `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '公告标题',
-  `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL COMMENT '公告内容',
-  `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
-  PRIMARY KEY (`id`) USING BTREE,
-  INDEX `fk_notice_user`(`user_id`) USING BTREE,
+  `title` varchar(255) NOT NULL COMMENT '公告标题',
+  `content` text COMMENT '公告内容',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`),
+  KEY `fk_notice_user` (`user_id`),
   CONSTRAINT `fk_notice_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Table structure for reply
--- ----------------------------
+--
+-- Table structure for table `reply`
+--
+
 DROP TABLE IF EXISTS `reply`;
-CREATE TABLE `reply`  (
-  `id` int(11) NOT NULL COMMENT '回复id',
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `reply` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '回复id',
   `topic_id` int(11) NOT NULL COMMENT '帖子id',
   `user_id` int(11) NOT NULL COMMENT '回复者用户id',
-  `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL COMMENT '回复内容',
-  `reply_time` datetime(0) NULL DEFAULT NULL COMMENT '回复时间',
-  PRIMARY KEY (`id`) USING BTREE,
-  INDEX `fk_reply_user`(`user_id`) USING BTREE,
-  INDEX `fk_reply_topic`(`topic_id`) USING BTREE,
+  `content` text COMMENT '回复内容',
+  `reply_time` datetime DEFAULT NULL COMMENT '回复时间',
+  PRIMARY KEY (`id`),
+  KEY `fk_reply_user` (`user_id`),
+  KEY `fk_reply_topic` (`topic_id`),
   CONSTRAINT `fk_reply_topic` FOREIGN KEY (`topic_id`) REFERENCES `topic` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `fk_reply_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Table structure for sub_section
--- ----------------------------
+--
+-- Table structure for table `sensitive_word`
+--
+
+DROP TABLE IF EXISTS `sensitive_word`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `sensitive_word` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `word` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `sub_section`
+--
+
 DROP TABLE IF EXISTS `sub_section`;
-CREATE TABLE `sub_section`  (
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `sub_section` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '子版块id',
   `main_section_id` int(11) NOT NULL COMMENT '对应的主版块id',
-  `title` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '子版块标题',
-  PRIMARY KEY (`id`) USING BTREE,
-  INDEX `fk_sub_main_section`(`main_section_id`) USING BTREE,
+  `title` varchar(50) NOT NULL COMMENT '子版块标题',
+  `info` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_sub_main_section` (`main_section_id`),
   CONSTRAINT `fk_sub_main_section` FOREIGN KEY (`main_section_id`) REFERENCES `main_section` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Table structure for topic
--- ----------------------------
+--
+-- Table structure for table `topic`
+--
+
 DROP TABLE IF EXISTS `topic`;
-CREATE TABLE `topic`  (
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `topic` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '帖子id',
   `section_id` int(11) NOT NULL COMMENT '对应子版块id',
   `user_id` int(11) NOT NULL COMMENT '发帖人id',
-  `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '标题',
-  `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '内容',
-  `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
-  `update_time` datetime(0) NULL DEFAULT NULL COMMENT '更新时间',
+  `title` varchar(255) NOT NULL COMMENT '标题',
+  `content` text NOT NULL COMMENT '内容',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime DEFAULT NULL COMMENT '更新时间',
   `click` int(11) NOT NULL COMMENT '点击量',
-  `reply_num` int(11) NULL DEFAULT NULL COMMENT '评论量',
-  `type` int(11) NULL DEFAULT NULL COMMENT '帖子类型,0为普通帖,1为精华帖',
-  PRIMARY KEY (`id`) USING BTREE,
-  INDEX `fk_topic_section`(`section_id`) USING BTREE,
-  INDEX `fk_topic_user`(`user_id`) USING BTREE,
+  `reply_num` int(11) DEFAULT NULL COMMENT '评论量',
+  `type` int(11) DEFAULT NULL COMMENT '帖子类型,0为普通帖,1为精华帖',
+  PRIMARY KEY (`id`),
+  KEY `fk_topic_section` (`section_id`),
+  KEY `fk_topic_user` (`user_id`),
   CONSTRAINT `fk_topic_section` FOREIGN KEY (`section_id`) REFERENCES `sub_section` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `fk_topic_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Table structure for user
--- ----------------------------
+--
+-- Table structure for table `user`
+--
+
 DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user`  (
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '用户唯一id',
-  `username` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '用户名',
-  `password` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '密码',
-  `sex` varchar(5) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '性别',
-  `email` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '邮箱',
-  `userAvatarURL` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '头像url',
-  `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
-  `update_time` datetime(0) NULL DEFAULT NULL COMMENT '更新时间',
+  `username` varchar(30) NOT NULL COMMENT '用户名',
+  `password` varchar(50) NOT NULL COMMENT '密码',
+  `sex` varchar(5) DEFAULT NULL COMMENT '性别',
+  `email` varchar(50) DEFAULT NULL COMMENT '邮箱',
+  `userAvatarURL` varchar(255) DEFAULT NULL COMMENT '头像url',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime DEFAULT NULL COMMENT '更新时间',
   `level` int(11) NOT NULL COMMENT '用户等级',
-  `active_code` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '激活码',
-  `hasActive` int(11) NULL DEFAULT NULL COMMENT '激活状态,0为未激活,1为激活',
-  `signature` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '签名',
-  `privilege` int(4) NULL DEFAULT NULL COMMENT '用户权限,0为普通用户,1为管理员',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+  `active_code` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '激活码',
+  `hasActive` int(11) DEFAULT NULL COMMENT '激活状态,0为未激活,1为激活',
+  `signature` varchar(255) DEFAULT NULL COMMENT '签名',
+  `privilege` int(4) DEFAULT NULL COMMENT '用户权限,0为普通用户,1为管理员',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-SET FOREIGN_KEY_CHECKS = 1;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-01-09  0:25:49
